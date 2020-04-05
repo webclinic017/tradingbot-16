0a666f1a2d4280816dec9210f4197b093c00cca2
Abhinav Pola <pola2@illinois.edu>
2020-04-04 21:38:32 -0500

    added trading abstraction

---
+++ trading_clients/AlpacaTrader.py
@@ -0,0 +1,12 @@
+from Trader import Trader
+
+class AlpacaTrader(Trader):
+    def get_account(self):
+        return api.get_account()
+    
+    def get_position(self, symbol):
+        return api.get_position(symbol)
+    
+    def submit_order(self, symbol, qty, side, type, time_in_force, 
+        limit_price=None, stop_price=None, client_order_id=None, order_class=None, take_profit=None, stop_loss=None):
+        return api.submit_order(symbol, qty, side, type, time_in_force)
