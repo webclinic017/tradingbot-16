e9df1c455bbd75c53890a503e27c0533fb7352d0
Abhinav Pola <pola2@illinois.edu>
2020-04-04 21:15:01 -0500

    added trading wrapper class

--- strategy/strategy.py
+++ strategy/strategy.py
@@ -2,6 +2,7 @@
 This is the main file that runs our strategy. All strategy assets should be placed
 in the strategy folder along with any helper files. 
 Strategy files are the only files with access to indicators, prices, stock lists, etc.
+It should not have any direct interactions with the brokerage api. Interactions are made via trading.py
 """
 
 def run():
@@ -9,3 +10,4 @@ def run():
 
 def update():
     return
+
---
+++ trading.py
@@ -0,0 +1,5 @@
+"""
+This should be a wrapper class for our interactions with the brokerage.
+For example, we write our own generic submit_order function that works more universally
+rather than using alpaca_api.submit_order
+"""
