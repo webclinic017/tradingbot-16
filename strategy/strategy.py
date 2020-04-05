e5181403f22c4468320c481c60f190985119142f
Abhinav Pola <pola2@illinois.edu>
2020-04-04 20:55:23 -0500

    added strategy template

---
+++ strategy/strategy.py
@@ -0,0 +1,11 @@
+""" 
+This is the main file that runs our strategy. All strategy assets should be placed
+in the strategy folder along with any helper files. 
+Strategy files are the only files with access to indicators, prices, stock lists, etc.
+"""
+
+def run():
+    return
+
+def update():
+    return
