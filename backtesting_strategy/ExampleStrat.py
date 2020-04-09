a85d16ad94527d77b4d5a36057a52bd47a2d2fc5
Sheel Sansare <sheelsansare@Sheels-MBP.attlocal.net>
2020-04-09 01:00:24 -0500

    reorganized backtrader

--- backtesting_strategy/ExampleStrat.py
+++ backtesting_strategy/ExampleStrat.py
@@ -1,13 +1,11 @@
+''' 
+This is the example strategy that the backtrader website used.
+ It buys when there are three consecutive green days and sells when 
+ there are three red days. 
+''' 
 
-import datetime  # For datetime objects
-import os.path  # To manage paths
-import sys  # To find out the script name (in argv[0])
-
-# Import the backtrader platform
 import backtrader as bt
 
-
-# Create a Stratey
 class TestStrategy(bt.Strategy):
     params = (
         ('exitbars', 5),
@@ -97,47 +95,3 @@ class TestStrategy(bt.Strategy):
 
                 # Keep track of the created order to avoid a 2nd order
                 self.order = self.sell()
-
-if __name__ == '__main__':
-    # Create a cerebro entity
-    cerebro = bt.Cerebro()
-
-    # Add a strategy
-    cerebro.addstrategy(TestStrategy)
-
-    # Datas are in a subfolder of the samples. Need to find where the script is
-    # because it could have been called from anywhere
-    modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
-    datapath = 'FB.csv'
-
-    # Create a Data Feed
-    data = bt.feeds.YahooFinanceCSVData(
-        dataname=datapath,
-        # Do not pass values before this date
-        fromdate=datetime.datetime(2017, 1, 1),
-        # Do not pass values before this date
-        todate=datetime.datetime(2017, 12, 31),
-        # Do not pass values after this date
-        reverse=False)
-
-    # Add the Data Feed to Cerebro
-    cerebro.adddata(data)
-
-    # Set our desired cash start
-    cerebro.broker.setcash(100000.0)
-
-    # Add a FixedSize sizer according to the stake
-    cerebro.addsizer(bt.sizers.FixedSize, stake=10)
-
-    # Set the commission - 0.1% ... divide by 100 to remove the %
-    cerebro.broker.setcommission(commission=0.000)
-
-    # Print out the starting conditions
-    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-
-    # Run over everything
-    cerebro.run()
-
-    # Print out the final result
-    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
-    cerebro.plot()
